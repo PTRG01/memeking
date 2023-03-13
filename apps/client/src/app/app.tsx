59cbbee71264c05a75a0a56eaeab5a7dcda16f93
PTRG01 <95378495+PTRG01@users.noreply.github.com>
2023-03-13 21:46:13 +0100

    feat: useAuth hook implemented

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,15 +1,32 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import {
+  AuthProvider,
+  useAuthContext,
+} from './contexts/auth-provider/auth-provider';
 
-import NxWelcome from './nx-welcome';
+const TestComponent = () => {
+  const { signIn } = useAuthContext();
+
+  return (
+    <button
+      onClick={() =>
+        signIn({
+          email: 'test@test.com',
+          password: 'test1234',
+        })
+      }
+    >
+      Login
+    </button>
+  );
+};
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="client" />
-
-      <div />
-    </>
+    <AuthProvider>
+        <TestComponent />
+      </AuthProvider>
   );
 }
 
---
+++ apps/client/src/app/contexts/auth-provider/auth-provider.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useContext, useMemo } from 'react';
+import { pb } from '../../utils/pocketbase';
+import { Record, Admin } from 'pocketbase';
+
+export type TSignInFunction = (params: {
+  email: string;
+  password: string;
+}) => Promise<void>;
+
+export type TSignUpFunction = (params: {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  username: string;
+  emailVisibility?: boolean;
+}) => Promise<void>;
+
+export type TLogoutFunction = () => void;
+
+export interface IUser {
+  email: string;
+  avatar: string;
+  id: string;
+  load: string;
+}
+
+export type TUserModel = Record | Admin | null;
+
+export interface IAuthContext {
+  signIn: TSignInFunction;
+  signUp: TSignUpFunction;
+  logout: TLogoutFunction;
+  user: TUserModel;
+  isLoggedIn: boolean;
+}
+
+const AuthContext = React.createContext<IAuthContext | null>(null);
+
+export const AuthProvider = ({ children }: React.PropsWithChildren) => {
+  const [user, setUser] = useState<TUserModel>(null);
+
+  const isLoggedIn = useMemo(() => !!user, [user]);
+
+  useEffect(() => {
+    const unregister = pb.authStore.onChange((token) => {
+      setUser(pb.authStore.model);
+    });
+
+    return () => {
+      unregister();
+    };
+  }, []);
+
+  const signUp: TSignUpFunction = async (params) => {
+    const authData = await pb.collection('users').create({
+      emailVisibility: true,
+      ...params,
+    });
+    console.log(authData);
+  };
+
+  const signIn: TSignInFunction = async ({ email, password }) => {
+    await pb.collection('users').authWithPassword(email, password);
+  };
+
+  const logout: TLogoutFunction = async () => {
+    pb.authStore.clear();
+  };
+
+  return (
+    <AuthContext.Provider value={{ signUp, signIn, logout, user, isLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const data = useContext(AuthContext);
+
+  if (!data) {
+    throw Error('useAuthContext should be used inside of AuthProvider');
+  }
+
+  return data;
+};
