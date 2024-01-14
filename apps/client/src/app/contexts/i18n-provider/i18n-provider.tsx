a584313aa8a43012b17e85a00d75197c01348c35
PTRG01 <95378495+PTRG01@users.noreply.github.com>
2024-01-14 19:19:40 +0100

    feat: french translation added

--- apps/client/src/app/contexts/i18n-provider/i18n-provider.tsx
+++ apps/client/src/app/contexts/i18n-provider/i18n-provider.tsx
@@ -3,11 +3,12 @@ import { initReactI18next } from 'react-i18next';
 import en from '../../config/i18n/messages/en';
 import pl from '../../config/i18n/messages/pl';
 import de from '../../config/i18n/messages/de';
+import fr from '../../config/i18n/messages/fr';
 
 /* eslint-disable-next-line */
 
 i18n.use(initReactI18next).init({
-  resources: { en, pl, de },
+  resources: { en, pl, de, fr },
   lng: 'en',
   fallbackLng: 'en',
   debug: true,
