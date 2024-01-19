9d196e5a8f789c460d9d19f7feb041a7c6fc91a8
PTRG01 <95378495+PTRG01@users.noreply.github.com>
2024-01-19 18:04:52 +0100

    fix: fix env files

--- apps/client/src/app/utils/pocketbase.ts
+++ apps/client/src/app/utils/pocketbase.ts
@@ -1,3 +1,5 @@
 import PocketBase from 'pocketbase';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 const url = import.meta.env.VITE_POCKETBASE_URL;
 export const pb = new PocketBase(url);
